462b7d6989357fab4e94a55bd9c82c2673dc35a0
viesemenova-cloud <viesemenova@gmail.com>
2026-06-16 12:32:54 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+  componentDidCatch(error) {
+    this.setState({ error: error.message });
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{ color: "white", padding: 20, fontFamily: "monospace" }}>
+          <h2>Ошибка:</h2>
+          <pre>{this.state.error}</pre>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
